3f81b0a896db2ca62d4bd0da96a010151a948819
Mack McQuen <mackmcquen@comcast.net>
2020-03-27 21:36:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { |dwarf, index| 
+    puts "#{index + 1}. #{dwarf}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|elem| "#{elem.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |elem| elem.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  index = 0
+  while index < array.length do
+    if cheese_types.detect (array[index])
+      return array[index]
+    else
+      index += 1
+    end
+  end
+  nil
 end
