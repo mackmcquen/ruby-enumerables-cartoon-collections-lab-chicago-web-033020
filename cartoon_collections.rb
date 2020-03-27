fefcc6e4adca569f60f5fa2a80ae5a5d083a416e
Mack McQuen <mackmcquen@comcast.net>
2020-03-27 21:40:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,8 @@
-
 def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each_with_index { |dwarf, index|
     puts "#{(index+1)}. #{dwarf.capitalize}\n"
-  }
+}
 end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
@@ -24,6 +23,6 @@ def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   array.find { |cheese|
-    cheese_types.include?(cheese)
-    }
+  cheese_types.include?(cheese)
+}
 end
